b6a6445fc762b9445f27e82ec50952b6946a3f81
Nemanja Stracenski <69650865+nemanjastracenski@users.noreply.github.com>
2021-05-03 23:02:13 +0200

    Add files via upload

---
+++ Basic_Greeter_Vampire.py
@@ -0,0 +1,13 @@
+print('Enter your name.')
+name = input()
+print('Enter your age.')
+age = input()
+
+if name == 'Alice':
+    print('Hi Alice!')
+elif age < 12:
+    print('You are not Alice.kiddo.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire')
+elif age > 100:
+    print('Your are not Alice,granie')
